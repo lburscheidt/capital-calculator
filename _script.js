aa6fbba6aab2aea0ebfce3ba41099e0e676d6bc0
L Burscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-08-29 16:29:46 +0200

    Remove decimal. Add support for multiple calculations in a row.

--- _script.js
+++ _script.js
@@ -1,195 +1,33 @@
-let num1;
-let num2;
-let operator;
-let display = document.getElementById('display');
-let operation = display.textContent;
-
-function add(num1, num2) {
-  return (display.textContent = num1 + num2);
-}
-function subtract(num1, num2) {
-  return (display.textContent = num1 - num2);
-}
-function multiply(num1, num2) {
-  return (display.textContent = num1 * num2);
-}
-function divide(num1, num2) {
-  if (num2 !== 0) {
-    return (display.textContent = num1 / num2);
-  } else {
-    return (display.innerHTML =
-      '<i class="fa-solid fa-skull"></i><i class="fa-solid fa-skull"></i><i class="fa-solid fa-skull"></i>');
-  }
-}
-
-function operate(num1, num2, operator) {
-  if (operator == '+') {
-    add(num1, num2);
-  } else if (operator == '-') {
-    subtract(num1, num2);
-  } else if (operator == '*') {
-    multiply(num1, num2);
-  } else if (operator == '/') {
-    divide(num1, num2);
-  }
-}
-
-/*populate display*/
-
-/*numbers*/
-
-let oneBtn = document.getElementById('1');
-let twoBtn = document.getElementById('2');
-let threeBtn = document.getElementById('3');
-let fourBtn = document.getElementById('4');
-let fiveBtn = document.getElementById('5');
-let sixBtn = document.getElementById('6');
-let sevenBtn = document.getElementById('7');
-let eightBtn = document.getElementById('8');
-let nineBtn = document.getElementById('9');
-let zeroBtn = document.getElementById('zero');
-
-/*operators*/
-let plusBtn = document.getElementById('+');
-let minusBtn = document.getElementById('-');
-let multiplyBtn = document.getElementById('*');
-let divideBtn = document.getElementById('/');
-let periodBtn = document.getElementById('.');
-let equalsBtn = document.getElementById('=');
-
-/*clear & backspace*/
-let clearBtn = document.getElementById('C');
-let backspaceBtn = document.getElementById('Del');
-
+let num1 = '';
+let num2 = '';
+let operator = '';
+let result = '';
 disableOperatorBtns();
 
-/*event listeners*/
-
-oneBtn.addEventListener('click', function () {
-  enableOperatorBtns();
-  displayAndStore('1');
-});
-twoBtn.addEventListener('click', function () {
-  enableOperatorBtns();
-  displayAndStore('2');
-});
-threeBtn.addEventListener('click', function () {
-  enableOperatorBtns();
-  displayAndStore('3');
-});
-fourBtn.addEventListener('click', function () {
-  enableOperatorBtns();
-  displayAndStore('4');
-});
-fiveBtn.addEventListener('click', function () {
-  enableOperatorBtns();
-  displayAndStore('5');
-});
-sixBtn.addEventListener('click', function () {
-  enableOperatorBtns();
-  displayAndStore('6');
-});
-sevenBtn.addEventListener('click', function () {
-  enableOperatorBtns();
-  displayAndStore('7');
-});
-eightBtn.addEventListener('click', function () {
-  enableOperatorBtns();
-  displayAndStore('8');
-});
-nineBtn.addEventListener('click', function () {
-  enableOperatorBtns();
-  displayAndStore('9');
-});
-zeroBtn.addEventListener('click', function () {
-  enableOperatorBtns();
-  displayAndStore('0');
-});
-
-plusBtn.addEventListener('click', function () {
-  displayAndStore('+');
-
-  disableOperatorBtns();
-});
-minusBtn.addEventListener('click', function () {
-  displayAndStore('-');
-  disableOperatorBtns();
-});
-multiplyBtn.addEventListener('click', function () {
-  displayAndStore('*');
-  disableOperatorBtns();
-});
-divideBtn.addEventListener('click', function () {
-  displayAndStore('/');
-  disableOperatorBtns();
-});
-
-equalsBtn.addEventListener('click', function () {
-  displayAndStore('=');
-
-  splitString(display.textContent);
-});
-
-periodBtn.addEventListener('click', function () {
-  displayAndStore('.');
-});
-
-clearBtn.addEventListener('click', clearDisplay);
-
-backspaceBtn.addEventListener('click', backspaceDelete);
-
-function disablePeriod() {
-  periodBtn.disabled = true;
+function add(num1, num2) {
+  result = num1 + num2;
 }
 
-function backspaceDelete() {
-  let text = display.innerText.replace(' ', '').slice(0, -1);
-  console.log(text);
-  display.innerText = text;
-  if ((plusBtn.disabled = true)) {
-    enableOperatorBtns();
-  }
+function subtract(num1, num2) {
+  result = num1 - num2;
 }
 
-function displayAndStore(str) {
-  /*store str input in variables*/
-  if (
-    display.textContent.includes('*') ||
-    display.textContent.includes('+') ||
-    display.textContent.includes('/') ||
-    display.textContent.includes('-')
-  ) {
-    num2 += str;
-  } else {
-    num1 += str;
-  }
-
-  if (
-    str == '1' ||
-    str == '2' ||
-    str == '3' ||
-    str == '4' ||
-    str == '5' ||
-    str == '6' ||
-    str == '7' ||
-    str == '8' ||
-    str == '9' ||
-    str == '0'
-  ) {
-    /*Add number to display*/
-    display.textContent += str;
-  } else if (str == '+' || str == '-' || str == '*' || str == '/') {
-    /*Add operator with spaces to display */
-    display.textContent += ' ' + str + ' ';
-    operator = str;
-  }
-  console.log(num1);
-  console.log(num2);
-  console.log(operator);
+function multiply(num1, num2) {
+  result = num1 * num2;
 }
 
-function clearDisplay() {
-  display.textContent = '';
+function divide(num1, num2) {
+  result = num1 / num2;
+}
+
+function putNumberInDisplay(num) {
+  display.textContent += num;
+}
+
+function putOperatorInDisplay(op) {
+  let opString = ' ' + op + ' ';
+  console.log(opString);
+  display.textContent += opString;
 }
 
 function disableOperatorBtns() {
@@ -204,25 +42,145 @@ function enableOperatorBtns() {
   });
 }
 
-function splitString(str) {
-  let characters = str.split(' ');
-  num1 = Number(characters[0]);
-  num2 = Number(characters[2]);
-  operator = characters[1];
-  console.log(num1);
-  console.log(num2);
-  console.log(operator);
-  operate(num1, num2, operator);
+let oneBtn = document.getElementById('1');
+let twoBtn = document.getElementById('2');
+let threeBtn = document.getElementById('3');
+let fourBtn = document.getElementById('4');
+let fiveBtn = document.getElementById('5');
+let sixBtn = document.getElementById('6');
+let sevenBtn = document.getElementById('7');
+let eightBtn = document.getElementById('8');
+let nineBtn = document.getElementById('9');
+let zeroBtn = document.getElementById('zero');
+
+let plusBtn = document.getElementById('+');
+let minusBtn = document.getElementById('-');
+let divideBtn = document.getElementById('/');
+let multiplyBtn = document.getElementById('*');
+let equalsBtn = document.getElementById('=');
+
+let clearBtn = document.getElementById('clear');
+let deleteBtn = document.getElementById('del');
+
+/*event listeners: numbers*/
+oneBtn.addEventListener('click', function () {
+  putNumberInDisplay('1');
+  enableOperatorBtns();
+});
+twoBtn.addEventListener('click', function () {
+  putNumberInDisplay('2');
+  enableOperatorBtns();
+});
+threeBtn.addEventListener('click', function () {
+  putNumberInDisplay('3');
+  enableOperatorBtns();
+});
+fourBtn.addEventListener('click', function () {
+  putNumberInDisplay('4');
+  enableOperatorBtns();
+});
+fiveBtn.addEventListener('click', function () {
+  putNumberInDisplay('5');
+  enableOperatorBtns();
+});
+sixBtn.addEventListener('click', function () {
+  putNumberInDisplay('6');
+  enableOperatorBtns();
+});
+sevenBtn.addEventListener('click', function () {
+  putNumberInDisplay('7');
+  enableOperatorBtns();
+});
+eightBtn.addEventListener('click', function () {
+  putNumberInDisplay('8');
+  enableOperatorBtns();
+});
+nineBtn.addEventListener('click', function () {
+  putNumberInDisplay('9');
+  enableOperatorBtns();
+});
+zeroBtn.addEventListener('click', function () {
+  putNumberInDisplay('0');
+  enableOperatorBtns();
+});
+
+/*event listeners: operations*/
+plusBtn.addEventListener('click', function () {
+  putOperatorInDisplay('+');
+  disableOperatorBtns();
+  saveVariables();
+});
+
+minusBtn.addEventListener('click', function () {
+  putOperatorInDisplay('-');
+  disableOperatorBtns();
+  saveVariables();
+});
+divideBtn.addEventListener('click', function () {
+  putOperatorInDisplay('/');
+  disableOperatorBtns();
+  saveVariables();
+});
+multiplyBtn.addEventListener('click', function () {
+  putOperatorInDisplay('*');
+  disableOperatorBtns();
+  saveVariables();
+});
+
+equalsBtn.addEventListener('click', function () {
+  putOperatorInDisplay('=');
+  disableOperatorBtns();
+  saveVariables();
+});
+
+/*event listeners: delete, clear */
+
+deleteBtn.addEventListener('click', backspaceDelete);
+clearBtn.addEventListener('click', clearDisplay);
+
+/*functions: delete, clear */
+
+function backspaceDelete() {
+  let text = display.textContent.replace(' ', '').slice(0, -1);
+  console.log(text);
+  display.textContent = text;
+  if ((plusBtn.disabled = true)) {
+    enableOperatorBtns();
+  }
+}
+function clearDisplay() {
+  display.textContent = '';
+}
+
+/*math logic */
+function saveVariables() {
+  let str = display.textContent.trimEnd().split(' ');
+  console.log(str);
+  if (str.length < 3) {
+    num1 = str[0];
+    operator = str[1];
+  } else {
+    num1 = Number(str[0]);
+    operator = str[1];
+    num2 = Number(str[2]);
+    operator2 = str[3];
+    operate(num1, num2, operator);
+    if (operator2 == '=') {
+      display.textContent = result;
+    } else {
+      display.textContent = result + ' ' + operator2 + ' ';
+    }
+  }
 }
 
 function operate(num1, num2, operator) {
   if (operator == '+') {
-    let result = add(num1, num2);
-  } else if (operator == '-') {
-    let result = subtract(num1, num2);
+    add(num1, num2);
+  } else if ((operator = '-')) {
+    subtract(num1, num2);
   } else if (operator == '*') {
-    let result = multiply(num1, num2);
+    multiply(num1, num2);
   } else if (operator == '/') {
-    let result = divide(num1, num2);
+    divide(num1, num2);
   }
 }
--- spec/spec.md
+++ spec/spec.md
@@ -11,6 +11,8 @@ Here are some use cases (abilities your project needs to have):
     A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
 
     Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+    
     Create a basic HTML calculator with buttons for each digit, each of the above functions and an “Equals” key.
         Do not worry about wiring up the JS just yet.
         There should also be a display for the calculator. Go ahead and fill it with some dummy numbers so it looks correct.
